54a4fa009d7708fc5a0e45ebc441c9d6274b30d1
WallSoGB <11296448+WallSoGB@users.noreply.github.com>
2026-03-04 11:47:03 +0100

    Test 2

--- PipBoyShadingFix/main.cpp
+++ PipBoyShadingFix/main.cpp
@@ -30,7 +30,7 @@ bool NVSEPlugin_Query(const NVSEInterface* nvse, PluginInfo* info)
 {
 	info->infoVersion = PluginInfo::kInfoVersion;
 	info->name = "PipBoyShadingFix";
-	info->version = 2;
+	info->version = 3;
 
 	return true;
 }
